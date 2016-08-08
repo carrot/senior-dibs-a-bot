c8cf0dcfeb10cf3f8ec785cc9edfbc4ff1e8f780
Brian Bowman <brian.bowman@carrotcreative.com>
2016-08-08 16:52:23 -0400

    refactor

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var Botkit = require('botkit')
 var Store = require('jfs')
 var NoLimit = require('nolimit')
 var nolimit = new NoLimit({ filename: 'sirdibsabot'})
-
 var token = process.env.SLACK_TOKEN
 var postChannel = 'C0AAFTJNS'
 var controller = Botkit.slackbot({
@@ -36,24 +35,38 @@ controller.on('direct_message', function (bot, message) {
   if (message.user == 'U09NPAG11' || message.user == 'U024H9QHP') {
     if (message.text.toUpperCase().indexOf('RESET') > -1) {
       nolimit = new NoLimit({ filename: 'sirdibsabot'})
-      bot.say({
-        text: 'All Disabilities have been reset.',
-        channel: postChannel
-      })
+      sendMessage(bot, 'All Disabilities have been reset.')
     }
   }
   if (validateUserDibsabilityDM(message)) {
-    bot.say({
-      text: '<@' + message.user + '>, You sure can dibs-a-bot. Dont forget you only get one. Once thats gone you must wait for the boss to give you the green light.',
-      channel: postChannel
-    })
+    sendMessage(bot, '<@' + message.user + '>, You sure can dibs-a-bot. Dont forget you only get one. Once thats gone you must wait for the boss to give you the green light.')
   } else {
-    bot.say({
-      text: 'Sorry <@' + message.user + '> you cant dibs-a-bot until the boss says so, and right now he says no.',
-      channel: postChannel
-    })
+    sendMessage(bot, 'Sorry <@' + message.user + '> you cant dibs-a-bot until the boss says so, and right now he says no.')
   }
 })
+
+function checkStatus (bot, message, whatElse, more) {
+  if (validateItemDibsability(message)) {
+    if (message.comment.comment.toUpperCase().indexOf('DIBS') > -1) {
+      if (validateUserDibsability(message)) {
+        nolimit.stash({ key: message.file_id, value: 1 })
+        nolimit.stash({ key: message.comment.user, value: 1 })
+        sendMessage(bot, 'Break out the BLUE LABEL!!! <@' + message.comment.user + '> just won something of great value. You may need to hire private security to guard your treasure. I wish you the best.')
+      } else {
+        sendMessage(bot, 'Sorry <@' + message.comment.user + '>, you cant dibs-a-bot until the boss says so, and right now he says no.')
+      }
+    }
+  } else {
+    sendMessage(bot, 'Sorry <@' + message.comment.user + '>, this item is claimed.')
+  }
+}
+
+function sendMessage (bot, text) {
+  bot.say({
+    text: message
+    channel: postChannel
+  })
+}
 
 function validateUserDibsability (message) {
   return (!nolimit.fetch({ key: message.comment.user }) || nolimit.fetch({ key: message.comment.user }) === 0)
@@ -65,29 +78,4 @@ function validateUserDibsabilityDM (message) {
 
 function validateItemDibsability (message) {
   return (!nolimit.fetch({ key: message.file_id }) || nolimit.fetch({ key: message.file_id }) === 0)
-}
-
-function checkStatus (bot, message, whatElse, more) {
-  if (validateItemDibsability(message)) {
-    if (message.comment.comment.toUpperCase().indexOf('DIBS') > -1) {
-      if (validateUserDibsability(message)) {
-        nolimit.stash({ key: message.file_id, value: 1 })
-        nolimit.stash({ key: message.comment.user, value: 1 })
-        bot.say({
-          text: 'Break out the BLUE LABEL!!! <@' + message.comment.user + '> just won something of great value. You may need to hire private security to guard your treasure. I wish you the best.',
-          channel: postChannel
-        })
-      } else {
-        bot.say({
-          text: 'Sorry <@' + message.comment.user + '>, you cant dibs-a-bot until the boss says so, and right now he says no.',
-          channel: postChannel
-        })
-      }
-    }
-  } else {
-    bot.say({
-      text: 'Sorry <@' + message.comment.user + '>, this item is claimed.',
-      channel: postChannel
-    })
-  }
 }
