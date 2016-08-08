b8fe9df0c99847763c4e41f27fd44043ae2623c3
Brian Bowman <brian.bowman@carrotcreative.com>
2016-08-08 16:00:24 -0400

    ready for test

--- index.js
+++ index.js
@@ -4,46 +4,93 @@ var Store = require('jfs')
 var NoLimit = require('nolimit')
 var nolimit = new NoLimit({ filename: 'sirdibsabot'})
 
+var token = process.env.SLACK_TOKEN
+
 var controller = Botkit.slackbot({
-  debug: false,
-  json_file_store: new Store(require('path').resolve(__dirname,"./lib/data.json"))
+  retry: Infinity,
+  debug: false
 })
 
-// connect the bot to a stream of messages
-controller.spawn({
-  token: process.env.SLACK_TOKEN
-}).startRTM()
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  beep.start(controller, { debug: true })
+}
 
+// connect the bot to a stream of messages
 controller.on('file_comment_added', checkStatus)
 
 // reply to a direct message
-controller.on('direct_message',function(bot,message) {
-  if (message.user == 'Chris P OR Brian B') { // Chris P has the power
+controller.on('direct_message', function (bot, message) {
+  console.log(message)
+  if (message.user == 'U09NPAG11' || message.user == 'U024H9QHP') {
     if (message.text.toUpperCase().indexOf('RESET') > -1) {
-      for (var key in nolimit) {
-        nolimit.stash( {key: key, value: 0})
-      }
+      nolimit = new NoLimit({ filename: 'sirdibsabot'})
+      bot.say({
+        text: 'All Disabilities have been reset.',
+        channel: 'C0AAFTJNS'
+      })
     }
   } else {
-    if (validateDibsability(message)) {
-      bot.reply(message, 'You sure can dibs-a-bot. Dont forget you only get one. Once thats gone you must wait for the boss to give you the green light.')
+    if (validateUserDibsability(message)) {
+      bot.say({
+        text: '<@' + message.user + '>, You sure can dibs-a-bot. Dont forget you only get one. Once thats gone you must wait for the boss to give you the green light.',
+        channel: 'C0AAFTJNS'
+      })
     } else {
-      bot.reply(message, 'Sorry you cant dibs-a-bot until the boss says so, and right now he says no.')
+      bot.say({
+        text: 'Sorry <@' + message.user + '> you cant dibs-a-bot until the boss says so, and right now he says no.',
+        channel: 'C0AAFTJNS'
+      })
     }
   }
 })
 
-function validateDibsability (message) {
-  return (!nolimit.fetch(message.user) || nolimit.fetch(message.user) === 0)
+function validateUserDibsability (message) {
+  return (!nolimit.fetch({ key: message.comment.user }) || nolimit.fetch({ key: message.comment.user }) === 0)
 }
 
-function checkStatus (bot, message) {
-  if (message.comment.text.toUpperCase().indexOf('DIBS') > -1) {
-    if (validateDibsability(message)) {
-      nolimit.stash( {key: message.comment.user, value: 1})
-      bot.reply(message, 'Break out the BLUE LABEL!!! @<'message.comment.user'> just won something of great value. You may need to hire private security to guard your treasure. I wish you the best.')
-    } else {
-      bot.reply(message, 'Sorry @<'message.comment.user'> you cant dibs-a-bot until the boss says so, and right now he says no.')
+function validateItemDibsability (message) {
+  return (!nolimit.fetch({ key: message.file_id }) || nolimit.fetch({ key: message.file_id }) === 0)
+}
+
+function checkStatus (bot, message, whatElse, more) {
+  console.log(message)
+  if (validateItemDibsability(message)) {
+    console.log('unclaimed item')
+    if (message.comment.comment.toUpperCase().indexOf('DIBS') > -1) {
+      if (validateUserDibsability(message)) {
+        console.log('unclaimed user')
+        nolimit.stash({ key: message.file_id, value: 1 })
+        nolimit.stash({ key: message.comment.user, value: 1 })
+        bot.say({
+          text: 'Break out the BLUE LABEL!!! <@' + message.comment.user + '> just won something of great value. You may need to hire private security to guard your treasure. I wish you the best.',
+          channel: 'C0AAFTJNS'
+        })
+      } else {
+        console.log('claimed user')
+        bot.say({
+          text: 'Sorry <@' + message.comment.user + '>, you cant dibs-a-bot until the boss says so, and right now he says no.',
+          channel: 'C0AAFTJNS'
+        })
+      }
     }
+  } else {
+    console.log('claimed item')
+    bot.say({
+      text: 'Sorry <@' + message.comment.user + '>, this item is claimed.',
+      channel: 'C0AAFTJNS'
+    })
   }
 }
