74c22ee72c17c406dcb73e817efa2737470ae5cd
Brian Bowman <brian.bowman@carrotcreative.com>
2016-08-08 16:00:24 -0400

    set

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 
 [![npm](http://img.shields.io/npm/v/OGDibs.svg?style=flat)](https://badge.fury.io/js/OGDibs)  [![dependencies](http://img.shields.io/gemnasium/2020Deception/OGDibs.svg?style=flat)](https://gemnasium.com/2020Deception/OGDibs)
 
-OGDibs G checks your status and decides if your claim to fame is legit. Otherwise you get shamed real quick.
+OGDibs aka Senior Dibs A Bot checks your status and decides if your claim to fame is legit. Otherwise you get shamed real quick.
 
 > **Note:** This project is in early development, and versioning is a little different. [Read this](http://markup.im/#q4_cRZ1Q) for more details.
 
 ### Why should you care?
 
-Write about why this project is important.
+Its important because people like to cheat in the free for all channel and the homies dont play that
 
 ### Installation
 
-Instructions on how to install.
+lol
 
 ### Usage
 
-How to use this project.
+Bots use themselves
 
 ### License & Contributing
 
--- index.js
+++ index.js
@@ -1,49 +1,106 @@
+require('dotenv').config({silent: true})
 var path = require('path')
 var Botkit = require('botkit')
 var Store = require('jfs')
+var beep = require('beepboop-botkit')
 var NoLimit = require('nolimit')
 var nolimit = new NoLimit({ filename: 'sirdibsabot'})
-
+var token = process.env.SLACK_TOKEN
+var postChannel = process.env.MAIN_CHANNEL
+var admin1 = process.env.ADMIN1
+var admin2 = process.env.ADMIN2
+var filename = process.env.FILENAME
 var controller = Botkit.slackbot({
-  debug: false,
-  json_file_store: new Store(require('path').resolve(__dirname,"./lib/data.json"))
+  retry: Infinity,
+  debug: false
 })
 
-// connect the bot to a stream of messages
-controller.spawn({
-  token: process.env.SLACK_TOKEN
-}).startRTM()
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  beep.start(controller, { debug: true })
+}
 
+// connect the bot to a stream of messages
 controller.on('file_comment_added', checkStatus)
 
+controller.on('direct_mention', function (bot, message) {
+  handleDirectAction(bot, message)
+})
+
 // reply to a direct message
-controller.on('direct_message',function(bot,message) {
-  if (message.user == 'Chris P OR Brian B') { // Chris P has the power
-    if (message.text.toUpperCase().indexOf('RESET') > -1) {
-      for (var key in nolimit) {
-        nolimit.stash( {key: key, value: 0})
+controller.on('direct_message', function (bot, message) {
+  handleDirectAction(bot, message)
+})
+
+function checkStatus (bot, message, whatElse, more) {
+  if (validateItemDibsability(message)) {
+    if (message.comment.comment.toUpperCase().indexOf('DIBS') > -1) {
+      if (validateUserDibsability(message)) {
+        nolimit.stash({ key: message.file_id, value: 1 })
+        nolimit.stash({ key: message.comment.user, value: 1 })
+        sendMessage(bot, randomWinningPhrase(message))
+      } else {
+        sendMessage(bot, 'Sorry <@' + message.comment.user + '>, you cant dibs-a-bot until the boss says so, and right now he says no.')
       }
     }
   } else {
-    if (validateDibsability(message)) {
-      bot.reply(message, 'You sure can dibs-a-bot. Dont forget you only get one. Once thats gone you must wait for the boss to give you the green light.')
-    } else {
-      bot.reply(message, 'Sorry you cant dibs-a-bot until the boss says so, and right now he says no.')
-    }
+    sendMessage(bot, 'Sorry <@' + message.comment.user + '>, this item is claimed.')
   }
-})
-
-function validateDibsability (message) {
-  return (!nolimit.fetch(message.user) || nolimit.fetch(message.user) === 0)
 }
 
-function checkStatus (bot, message) {
-  if (message.comment.text.toUpperCase().indexOf('DIBS') > -1) {
-    if (validateDibsability(message)) {
-      nolimit.stash( {key: message.comment.user, value: 1})
-      bot.reply(message, 'Break out the BLUE LABEL!!! @<'message.comment.user'> just won something of great value. You may need to hire private security to guard your treasure. I wish you the best.')
-    } else {
-      bot.reply(message, 'Sorry @<'message.comment.user'> you cant dibs-a-bot until the boss says so, and right now he says no.')
+function randomWinningPhrase (message) {
+  var phrases = [
+    'Break out the BLUE LABEL!!! <@' + message.comment.user + '> is the winner.',
+    'Congrats <@' + message.comment.user + '>, ' + admin1 + ' doesn’t want something anymore and you\'re closer than a trash can.',
+    'Hey Hey Hey <@' + message.comment.user + '>! Enjoy the trash that someone else didn\'t want.',
+    'Well aren\'t you lucky <@' + message.comment.user + '>! You\'ve won at thing of semi-value.',
+    'Whoa <@' + message.comment.user + '>!, thank you for slightly reducing the Fire Hazard around ' + admin1
+  ]
+  var index = Math.floor((Math.random() * 4) + 0)
+  return phrases[index]
+}
+
+function sendMessage (bot, text) {
+  bot.say({
+    text: text,
+    channel: postChannel
+  })
+}
+
+function handleDirectAction (bot, message) {
+  if (message.user == admin1 || message.user == admin2) {
+    if (message.text.toUpperCase().indexOf('RESET') > -1) {
+      nolimit = new NoLimit({ filename: filename})
+      sendMessage(bot, 'All Dibsabilities have been reset.')
     }
   }
+  if (validateUserDibsabilityDM (message)) {
+    sendMessage(bot, '<@' + message.user + '>, you sure can dibs-a-bot. Dont forget you only get one. Once thats gone you must wait for the boss to give you the green light.')
+  } else {
+    sendMessage(bot, 'Sorry <@' + message.user + '>, you cant dibs-a-bot until the boss says so, and right now he says no.')
+  }
+}
+
+function validateUserDibsability (message) {
+  return (!nolimit.fetch({ key: message.comment.user }) || nolimit.fetch({ key: message.comment.user }) === 0)
+}
+
+function validateUserDibsabilityDM (message) {
+  return (!nolimit.fetch({ key: message.user }) || nolimit.fetch({ key: message.user }) === 0)
+}
+
+function validateItemDibsability (message) {
+  return (!nolimit.fetch({ key: message.file_id }) || nolimit.fetch({ key: message.file_id }) === 0)
 }
